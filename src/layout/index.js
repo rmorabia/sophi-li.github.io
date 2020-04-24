5858a9dfafc3c3887f19a66938131cf68088dc08
Sophia Li <sophiali124@gmail.com>
2020-04-23 22:23:25 -0700

    add favicon

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
 import 'modern-normalize'
 
 import './index.css'
@@ -21,6 +22,10 @@ const RootLayout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Helmet>
+          <title>{data.site.siteMetadata.title}</title>
+          <link rel="icon" type="image/png" href="/favicon.JPG" />
+        </Helmet>
         <Header title={data.site.siteMetadata.title} />
         <Container>
           {children}
