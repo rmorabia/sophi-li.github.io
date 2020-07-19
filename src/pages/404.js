56c70d4639a9352d95ef37c4566fa687cabde84b
Sophia Li <sophiali124@gmail.com>
2020-07-18 18:30:41 -0700

    fix left margin for all pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,13 @@
 import React from 'react'
+
 import Layout from '../layout'
 
-const NotFound = () => <Layout>Oops! Page not found.</Layout>
+import styles from './404.module.css'
+
+const NotFound = () => (
+  <Layout>
+    <div className={styles.container}>Oops! Page not found.</div>
+  </Layout>
+)
 
 export default NotFound
