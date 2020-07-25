ed05bbf8f75ab94267cd6d39df0244ed6879f2b2
Sophia Li <sophiali124@gmail.com>
2020-07-24 18:06:47 -0700

    add bog search feature

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql, Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
@@ -10,32 +10,77 @@ const Blog = ({
   data: {
     allMarkdownRemark: { edges },
   },
-}) => (
-  <Layout>
-    <h1 className="header">Blog</h1>
-    {edges.map(({ node: { frontmatter } }) => (
-      <div key={frontmatter.path}>
-        <Link to={frontmatter.path} className="linkContainer">
-          <div className="blogContainer">
-            <div className="titleTagContainer">
-              <p className="blogTitle">{frontmatter.title}</p>{' '}
-              <div className="blogTags">
-                {frontmatter.tags.map(tag => (
-                  <p className={`tag tag-${tag}`} key={tag}>
-                    {tag}
-                  </p>
-                ))}
+}) => {
+  const allPosts = edges
+  const emptyQuery = ''
+
+  const [state, setState] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  })
+
+  const handleInputChange = event => {
+    const query = event.target.value
+    const posts = edges || []
+
+    const filteredData = posts.filter(post => {
+      const { title, tags } = post.node.frontmatter
+      return (
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        (tags &&
+          tags
+            .join('')
+            .toLowerCase()
+            .includes(query.toLowerCase()))
+      )
+    })
+    setState({ query, filteredData })
+  }
+
+  const { filteredData, query } = state
+  const hasSearchResults = filteredData && query !== emptyQuery
+  const posts = hasSearchResults ? filteredData : allPosts
+
+  return (
+    <Layout>
+      <div className="blogPageContainer">
+        <h1 className="header">Blog</h1>
+        <p>
+          Mostly JavaScript, insights from coding projects, and other thoughts.
+        </p>
+        <form>
+          <input
+            className="input"
+            onChange={handleInputChange}
+            placeholder="Type here to search the blog..."
+          />
+        </form>
+
+        {posts.map(({ node: { frontmatter } }) => (
+          <div key={frontmatter.path}>
+            <Link to={frontmatter.path} className="linkContainer">
+              <div className="blogContainer">
+                <div className="titleTagContainer">
+                  <p className="blogTitle">{frontmatter.title}</p>{' '}
+                  <div className="blogTags">
+                    {frontmatter.tags.map(tag => (
+                      <p className={`tag tag-${tag}`} key={tag}>
+                        {tag}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+                <div className="dateContainer">
+                  <p className="blogDate">{frontmatter.date}</p>
+                </div>
               </div>
-            </div>
-            <div className="dateContainer">
-              <p className="blogDate">{frontmatter.date}</p>
-            </div>
+            </Link>
           </div>
-        </Link>
+        ))}
       </div>
-    ))}
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 Blog.propTypes = {
   data: PropTypes.shape({
@@ -46,6 +91,7 @@ Blog.propTypes = {
             frontmatter: PropTypes.shape({
               title: PropTypes.string,
               path: PropTypes.string,
+              tag: PropTypes.string,
               date: PropTypes.string,
             }),
           }),
