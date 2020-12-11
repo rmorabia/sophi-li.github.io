b1adb619a8bc604da172ee94fe9619471fbdac90
Sophia Li <sophiali124@gmail.com>
2020-12-10 20:13:37 -0800

    update blog title styles

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types'
 import './Posts.css'
 
 const Posts = ({ frontmatter }) => {
+  const dateArr = frontmatter.date.split(' ')
+  dateArr[0] = dateArr[0].slice(0, 3)
+  dateArr[1] = parseInt(dateArr[1], 10)
+  const formattedDate = `${dateArr[0]} ${dateArr[1]}, ${dateArr[2]}`
   return (
     <div>
       <Link to={frontmatter.path} className="linkContainer">
@@ -13,7 +17,7 @@ const Posts = ({ frontmatter }) => {
             <p className="blogTitle">{frontmatter.title}</p>
           </div>
           <div className="dateContainer">
-            <p className="blogDate">{frontmatter.date}</p>
+            <p className="blogDate">{formattedDate}</p>
           </div>
         </div>
       </Link>
